e58077fe52d97954dbca178ac8357d4356ba6c43
Stanislav Demydiuk <s.demydiuk@gmail.com>
2018-04-15 18:21:41 +0300

    Add zone2 support (based on reboon) implementation

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# domoticz-yamaha
+# Yamaha AV Receiver - Domoticz Python Plugin
 Python plugin for Domoticz to control Yamaha AV Receivers (Aventage Series) via TCP/IP 
 ![devices](images/devices.png?raw=true "Devices")
+
+## Installation
+
+1. Clone repository into your domoticz plugins folder
+```
+cd domoticz/plugins
+git clone https://github.com/stas-demydiuk/domoticz-yamaha-plugin yamaha-av-receiver
+```
+2. Restart domoticz
+3. Go to "Hardware" page and add new item with type "Yamaha AV Receiver"
+
+## Devices
+
+Plugin creates 6 devices:
+| Name         | Description                                                              |
+|--------------|--------------------------------------------------------------------------|
+| Volume Main  | Controls the volume of main zone and allows to switch it ON/OFF          |
+| Input Main   | Controls the input of main zone                                          |
+| Volume Zone2 | Controls the volume of zone2 and allows to switch it ON/OFF              |
+| Input Zone2  | Controls the input of zone2                                              |
+| Party Status | Shows and switches Party mode ON/Off and shows Surround decode           |
+| Volume Party | Controls the volume of both zones and allows to switch Party mode ON/OFF |
--- plugin.py
+++ plugin.py
@@ -10,7 +10,7 @@
      <param field="Mode6" label="Debug" width="75px">
        <options>
           <option label="True" value="Debug"/>
-          <option label="False" value="Normal"  default="true" />
+          <option label="False" value="Normal" default="true" />
        </options>
      </param>
     </params>
@@ -19,12 +19,48 @@
 import Domoticz
 import base64
 
+class Zone:
+    def __init__(self, inputDeviceUnit, volumeDeviceUnit):
+        self.active = False
+        self.input = ''
+        self.volume = ''
+        self.inputDevice = Devices[inputDeviceUnit]
+        self.volumeDevice = Devices[volumeDeviceUnit]
+        self.inputDeviceUnit = inputDeviceUnit
+        self.volumeDeviceUnit = volumeDeviceUnit
+
+    def setActive(self, isActive):
+        nValue = 1 if isActive else 0
+        UpdateDevice(self.volumeDeviceUnit, nValue, self.volumeDevice.sValue)
+        UpdateDevice(self.inputDeviceUnit, nValue, self.inputDevice.sValue)
+
+    def setVolume(self, dB):
+        vol = float(dB)
+        volume = int(vol*5/4 + 100)  # Min -80 db, max -0 db
+
+        UpdateDevice(self.volumeDeviceUnit, self.inputDevice.nValue, volume)
+
+    def setInput(self, inputName):
+        inputDevice = self.inputDevice
+
+        if inputDevice.Options:
+            listLevelNames = inputDevice.Options["LevelNames"].split("|")
+            count = 0
+            for levelName in listLevelNames:
+                if (levelName == inputName):
+                    self.input = str(int(count))
+                    break
+                count += 10
+
+        UpdateDevice(self.inputDeviceUnit, inputDevice.nValue, self.input)
+
+
 class BasePlugin:
     enabled = False
     isConnected = True
     outstandingPings = 0
     nextConnect = 0
-    commandArray = ["@MAIN:PWR=?", "@MAIN:VOL=?", "@MAIN:INP=?", "@MAIN:MUTE=?", "@MAIN:SOUNDPRG=?"]
+    commandArray = ["@MAIN:PWR=?", "@ZONE2:PWR=?", "@MAIN:SOUNDPRG=?", "@MAIN:VOL=?", "@MAIN:INP=?", "@MAIN:MUTE=?", "@ZONE2:VOL=?", "@ZONE2:MUTE=?", "@ZONE2:INP=?", "@SYS:PARTY=?"]
 
     def __init__(self):
         #self.var = 123
@@ -33,31 +69,34 @@ class BasePlugin:
     def onStart(self):
         if Parameters["Mode6"] == "Debug":
            Domoticz.Debugging(1)
+
         Domoticz.Debug("onStart called")
+        inputControlOptions = { 
+            "LevelActions" : "||||||||||||||||",
+            "LevelNames"   : "Off|HDMI1|HDMI2|HDMI3|HDMI4|HDMI5|HDMI6|HDMI7|Spotify|TUNER|Airplay|AV1|AV2|AV3|AV4|AV5|AV6|NET RADIO",
+            "LevelOffHidden" : "true",
+            "SelectorStyle" : "1" 
+        }
+
         if 1 not in Devices: 
-            Domoticz.Debug("Create Status Device")
-            Domoticz.Device(Name="Status",  Unit=1, Type=17,  Switchtype=17, Used=1).Create()          
+            Domoticz.Debug("Create Party Status Device")
+            Domoticz.Device(Name="Party Status", Unit=1, Type=17,  Switchtype=17, Used=1).Create()          
         if 2 not in Devices: 
-            Domoticz.Debug("Create Volume Device")
-            Domoticz.Device(Name="Volume",  Unit=2, Type=244, Subtype=73, Switchtype=7,  Image=8, Used=1).Create()
+            Domoticz.Debug("Create Volume Device - Main")
+            Domoticz.Device(Name="Volume Main", Unit=2, Type=244, Subtype=73, Switchtype=7, Image=8, Used=1).Create()
         if 3 not in Devices: 
-            Domoticz.Debug("Create HDMI Device")
-            Options = { "LevelActions" : "|||||||",
-                        "LevelNames"   : "Off|HDMI1|HDMI2|HDMI3|HDMI4|HDMI5|HDMI6|HDMI7",
-                        "LevelOffHidden" : "true",
-                        "SelectorStyle" : "0" 
-                      }
-            Domoticz.Device(Name="Source", Unit=3, TypeName="Selector Switch", Switchtype=18, Options=Options, Used=1).Create()
-        if 4 not in Devices:
-            Domoticz.Debug("Create AV Device")
-            Options = { "LevelActions" : "||||||",
-                        "LevelNames"   : "Off|AV1|AV2|AV3|AV4|AV5|AV6",
-                        "LevelOffHidden" : "true",
-                        "SelectorStyle" : "0"
-                      }
-            Domoticz.Device(Name="AV Source", Unit=4, TypeName="Selector Switch", Switchtype=18, Options=Options, Used=1).Create() 
-
-
+            Domoticz.Debug("Create Input Device - Main")
+            Domoticz.Device(Name="Input Main", Unit=3, TypeName="Selector Switch", Options=inputControlOptions, Used=1).Create()
+        if 4 not in Devices: 
+            Domoticz.Debug("Create Volume Device - Zone2")
+            Domoticz.Device(Name="Volume Zone2", Unit=4, Type=244, Subtype=73, Switchtype=7, Image=8, Used=1).Create()
+        if 5 not in Devices: 
+            Domoticz.Debug("Create Input Device - Zone2")
+            Domoticz.Device(Name="Input Zone2", Unit=5, TypeName="Selector Switch", Options=inputControlOptions, Used=1).Create()        
+        if 6 not in Devices: 
+            Domoticz.Debug("Create Volume Device - Party")
+            Domoticz.Device(Name="Volume Party", Unit=6, Type=244, Subtype=73, Switchtype=7, Image=8, Used=1).Create()
+            
         self.connection = Domoticz.Connection(Name="Yamaha connection", Transport="TCP/IP", Protocol="Line", Address=Parameters["Address"], Port=Parameters["Port"])
         self.connection.Connect()
         Domoticz.Heartbeat(20)
@@ -77,61 +116,127 @@ class BasePlugin:
         Domoticz.Debug("onMessage called")
         self.outstandingPings = self.outstandingPings - 1
         strData = Data.decode("utf-8", "ignore")
+        Domoticz.Debug(strData)
+
+        mainZone = Zone(3, 2)
+        zone2 = Zone(5, 4)
+
         for line in strData.splitlines():
             arrData = line.split('=')
-            for x in arrData:
-                Domoticz.Debug(x)
-            if (arrData[0] == "@MAIN:PWR"):
-                if (arrData[1] == "On"):
-                    UpdateDevice(1, 1, Devices[1].sValue)
-                elif (arrData[1] == "Standby"):
-                    UpdateDevice(1, 0, Devices[1].sValue)
-            elif (arrData[0] == "@MAIN:VOL"):
-                vol = float(arrData[1])
-                sliderValue = int(vol*5/4 + 100)
-                UpdateDevice(2, Devices[2].nValue, str(sliderValue))
-            elif (arrData[0] == "@MAIN:MUTE"):
-                if (arrData[1] == "Off"):
-                    UpdateDevice(2, 2, Devices[2].sValue)
-                elif (arrData[1] == "On"):
-                    UpdateDevice(2, 0, Devices[2].sValue)
-            elif (arrData[0] == "@MAIN:INP"):
-                s = arrData[1]
-                if (s.startswith("HDMI")):
-                  UpdateDevice(3,2,str(int(s[-1:])*10))
-                  UpdateDevice(4,2,"0")
-                elif (s.startswith("AV")):  
-                  UpdateDevice(4,2,str(int(s[-1:])*10))
-                  UpdateDevice(3,2,"0")
+            if (arrData[0] == "@SYS:PARTY"):
+                if (arrData[1] == "On") and (Devices[1].nValue != 1):
+                    UpdateDevice(1, 1, "PARTY - " + Devices[1].sValue)
+                elif (arrData[1] == "Off") and (Devices[1].nValue != 0):
+                    UpdateDevice(1, 0, Devices[1].sValue.replace("PARTY - ", ""))
             elif (arrData[0] == "@MAIN:SOUNDPRG"):
-                UpdateDevice(1, Devices[1].nValue, arrData[1])
-
+                if (Devices[1].nValue == 0):
+                    sValue = arrData[1]
+                else:
+                    sValue = "PARTY - " + arrData[1]
+                if (Devices[1].sValue != sValue):
+                    UpdateDevice(1, Devices[1].nValue, sValue)
+            elif (arrData[0] == "@MAIN:PWR"):
+                mainZone.setActive(arrData[1] == "On")
+            elif (arrData[0] == "@MAIN:INP"):
+                mainZone.setInput(arrData[1])
+            elif (arrData[0] == "@MAIN:VOL"):
+                mainZone.setVolume(arrData[1])
+            # elif (arrData[0] == "@MAIN:MUTE"):
+            #     mainZone.setActive(arrData[1] == "Off")
+            elif (arrData[0] == "@ZONE2:PWR"):
+                zone2.setActive(arrData[1] == "On")
+            elif (arrData[0] == "@ZONE2:INP"):
+                zone2.setInput(arrData[1])
+            elif (arrData[0] == "@ZONE2:VOL"):
+                zone2.setVolume(arrData[1])
+            # elif (arrData[0] == "@ZONE2:MUTE"):
+            #     zone2.setActive(arrData[1] == "Off")
+                
+        if (Devices[3].nValue == 0) and (Devices[5].nValue == 0) and (Devices[6].nValue != 0): # If both zones OFF, set Party OFF
+            UpdateDevice(6, 0, 0)
+                    
     def onCommand(self, Unit, Command, Level, Hue):
-        Domoticz.Debug("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
+        Domoticz.Debug("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level) + "', Hue: " + str(Hue))
         if (self.isConnected == False):
             self.connection.Connect() 
             return
-        if (Unit == 1):
-            if (Command == "Off"):
-                self.connection.Send("@MAIN:PWR=Standby\r\n")
-            elif (Command == "On"): 
-               self.connection.Send("@MAIN:PWR=On\r\n")
-        elif (Unit == 2): 
+        
+        if (Unit == 1):  # Display (Party) Status and switch PARTY on/off
+           if (Command == "Off"):
+                self.connection.Send("@SYS:PARTY=Off\r\n")
+           elif (Command == "On"): 
+                self.connection.Send("@SYS:PARTY=On\r\n")
+          
+        elif (Unit == 2): # Main Volume, mute and ON/OFF
             if (Command == "Set Level"): 
-                volume = int(Level)*4/5 - 80
-                volumeToSend = round(2*volume)/2
-                self.connection.Send("@MAIN:VOL="+str(volumeToSend)+"\r\n") 
+                volume = int(Level)*4/5 - 80   # Min -80 db, max 0 db
+                if int(Level) == 0: # Set Mute on if slider at Min
+                    self.connection.Send("@MAIN:MUTE=On\r\n")
+                else:
+                    volumeToSend = round(2*volume)/2  
+                    self.connection.Send("@MAIN:VOL="+str(volumeToSend)+"\r\n")
+                
             elif (Command == "Off"): 
-                self.connection.Send("@MAIN:MUTE=On\r\n") 
+                 self.connection.Send("@MAIN:PWR=Standby\r\n")
             elif (Command == "On"): 
-                self.connection.Send("@MAIN:MUTE=Off\r\n") 
-        elif (Unit == 3): 
-            input = str(int(int(Level)/10))
-            self.connection.Send("@MAIN:INP=HDMI" + input + "\r\n") 
-        elif (Unit == 4):
-            input = str(int(int(Level)/10))
-            self.connection.Send("@MAIN:INP=AV" + input + "\r\n")
+                 self.connection.Send("@MAIN:PWR=On\r\n")
+                 
+        elif (Unit == 3): # Main Input selection
+            if Devices[Unit].Options:
+                listLevelNames = Devices[Unit].Options["LevelNames"].split("|")
+                NewLevel = int(int(Level)/10)
+                listInput = listLevelNames[NewLevel].split(" ")
+                input = str(listInput[0]) #First element (text before space) must be Input
+                self.connection.Send("@MAIN:INP=" + input + "\r\n")
+                if str(listInput[0]) == "Off": # Also used to turn OFF
+                    self.connection.Send("@MAIN:PWR=Standby\r\n")
 
+        elif (Unit == 4): # ZONE2 Volume, mute and ON/OFF 
+            if (Command == "Set Level"): 
+              #  volume = int(Level)*4/10 - 60 # Min -60 db, max -20 db
+                volume = int(Level)*4/5 - 80   # Min -80 db, max 0 db
+                if int(Level) == 0: # Set Mute on if slider at Min
+                    self.connection.Send("@ZONE2:MUTE=On\r\n")
+                else:
+                    volumeToSend = round(2*volume)/2  
+                    self.connection.Send("@ZONE2:VOL="+str(volumeToSend)+"\r\n")
+                
+            elif (Command == "Off"): 
+                 self.connection.Send("@ZONE2:PWR=Standby\r\n")
+            elif (Command == "On"): 
+                 self.connection.Send("@ZONE2:PWR=On\r\n")
+                 
+        elif (Unit == 5): # ZONE2 Input selection
+            if Devices[Unit].Options:
+                listLevelNames = Devices[Unit].Options["LevelNames"].split("|")
+                NewLevel = int(int(Level)/10)
+                listInput = listLevelNames[NewLevel].split(" ")
+                input = str(listInput[0]) #First element (text before space) must be Input
+                self.connection.Send("@ZONE2:INP=" + input + "\r\n")
+                if str(listInput[0]) == "Off": # Also used to turn OFF
+                    self.connection.Send("@ZONE2:PWR=Standby\r\n")
+
+        elif (Unit == 6): # Dual Volume, mute and ON/ All OFF
+            if (Command == "Set Level"): 
+                volume = int(Level)*4/5 - 80   # Min -80 db, max 0 db
+                if int(Level) == 0: # Set Mute on if slider at Min
+                    self.connection.Send("@ZONE2:MUTE=On\r\n")
+                    self.connection.Send("@MAIN:MUTE=On\r\n")
+                else:
+                    volumeToSend = round(2*volume)/2
+                    volMain  = round(2*int(Devices[2].sValue)*4/5 - 80)/2
+                    volZone2 = round(2*int(Devices[4].sValue)*4/5 - 80)/2
+                    volDifference = volMain - volZone2 # Main Vol - Zone2 Vol
+                    self.connection.Send("@MAIN:VOL="+str(volumeToSend)+"\r\n")
+                    volumeToSend -= volDifference
+                    self.connection.Send("@ZONE2:VOL="+str(volumeToSend)+"\r\n")
+            elif (Command == "Off"): 
+                 self.connection.Send("@MAIN:PWR=Standby\r\n")
+                 self.connection.Send("@ZONE2:PWR=Standby\r\n")
+                 self.connection.Send("@SYS:PWR=Standby\r\n")
+            elif (Command == "On"):
+                 self.connection.Send("@MAIN:PWR=On\r\n")
+                 
     def onNotification(self, Data):
         Domoticz.Debug("onNotification: " + str(Data))
 
@@ -195,9 +300,9 @@ def onHeartbeat():
 def UpdateDevice(Unit, nValue, sValue):
     # Make sure that the Domoticz device still exists (they can be deleted) before updating it 
     if (Unit in Devices):
-        if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue):
+        if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != str(sValue)):
+            Domoticz.Log("Update " + str(Devices[Unit].nValue) + " -> " + str(nValue)+",'" + Devices[Unit].sValue + "' => '"+str(sValue)+"' ("+Devices[Unit].Name+")")
             Devices[Unit].Update(nValue, str(sValue))
-            Domoticz.Log("Update "+str(nValue)+":'"+str(sValue)+"' ("+Devices[Unit].Name+")")
     return
 
     # Generic helper functions
@@ -210,9 +315,10 @@ def DumpConfigToLog():
         Domoticz.Debug("Device:           " + str(x) + " - " + str(Devices[x]))
         Domoticz.Debug("Device ID:       '" + str(Devices[x].ID) + "'")
         Domoticz.Debug("Device Name:     '" + Devices[x].Name + "'")
+        Domoticz.Debug("Device DeviceID:  " + str(Devices[x].DeviceID))
         Domoticz.Debug("Device nValue:    " + str(Devices[x].nValue))
         Domoticz.Debug("Device sValue:   '" + Devices[x].sValue + "'")
-        Domoticz.Debug("Device LastLevel: " + str(Devices[x].LastLevel))
+        Domoticz.Debug("Device LastLevel: " + str(Devices[x].LastLevel)) 
     return
 
 def stringToBase64(s):
